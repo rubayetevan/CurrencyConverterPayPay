745fcb885227878b75f43bc2656ee04db2f078d4
Md. Rubayet Hassan <rubayet.evan@gmail.com>
2022-06-09 01:44:34 +0600

    Junit Test added in ValidatorsTest.kt for Validators.kt

--- app/src/main/java/com/codesignal/paypay/currencyconverter/common/utility/Validators.kt
+++ app/src/main/java/com/codesignal/paypay/currencyconverter/common/utility/Validators.kt
@@ -2,8 +2,6 @@ package com.codesignal.paypay.currencyconverter.common.utility
 
 class Validators {
     fun validateDecimalInput(s:String):Boolean{
-        return if(s.isEmpty()||s.isBlank()){
-            false
-        }else s != "."
+        return s.toDoubleOrNull() != null
     }
 }
---
+++ app/src/test/java/com/codesignal/paypay/currencyconverter/common/utility/ValidatorsTest.kt
@@ -0,0 +1,50 @@
+package com.codesignal.paypay.currencyconverter.common.utility
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+
+internal class ValidatorsTest {
+    private lateinit var validators:Validators
+
+    @Before
+    fun setup(){
+        validators = Validators()
+    }
+
+    @Test
+    fun validateDecimalInputInvalidParamTest1() {
+        val expected = false
+        val actual = validators.validateDecimalInput(".")
+        assertEquals(expected,actual)
+    }
+
+    @Test
+    fun validateDecimalInputInvalidParamTest2() {
+        val expected = false
+        val actual = validators.validateDecimalInput("")
+        assertEquals(expected,actual)
+    }
+
+    @Test
+    fun validateDecimalInputInvalidParamTest3() {
+        val expected = false
+        val actual = validators.validateDecimalInput("   ")
+        assertEquals(expected,actual)
+    }
+
+    @Test
+    fun validateDecimalInputValidParamTest1() {
+        val expected = true
+        val actual = validators.validateDecimalInput(" .1")
+        assertEquals(expected,actual)
+    }
+
+    @Test
+    fun validateDecimalInputValidParamTest2() {
+        val expected = true
+        val actual = validators.validateDecimalInput("1.1 ")
+        assertEquals(expected,actual)
+    }
+}
